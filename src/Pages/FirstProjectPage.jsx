5c36ec4ef5d97aca604c18475800492ca0d1d2f4
dropADuce <dropaduce@yandex.ru>
2021-11-09 02:14:14 +0500

    Добавил базовую навигацию

---
+++ src/Pages/FirstProjectPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "../Components/Navbar";
+
+const FirstProjectPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>
+                Тут будет первая 3д сцена!
+            </h1>
+        </div>
+    );
+};
+
+export default FirstProjectPage;
